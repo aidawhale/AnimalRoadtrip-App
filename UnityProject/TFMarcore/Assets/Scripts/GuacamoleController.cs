9e9c09d6c9abcd0c017c7c4808ce98f9468adfc9
Aida <aida-vidal@hotmail.com>
2020-06-23 12:27:20 +0200

    Add new Guacamole scene and init scripts

---
+++ UnityProject/TFMarcore/Assets/Scripts/GuacamoleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuacamoleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void SetScene()
+    {
+
+    }
+
+}
